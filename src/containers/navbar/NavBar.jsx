7dd337c35b82daa94fb0491a8feaa09c5899617a
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2024-02-04 17:55:23 +0100

    Upgrading Navigation Bar to be reactive

--- src/containers/navbar/NavBar.jsx
+++ src/containers/navbar/NavBar.jsx
@@ -1,25 +1,49 @@
 import './navbar.css';
-import React from "react";
+import React, {useState} from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from '../../assets/logoDocWire.JPG'
 
+    const Menu = () =>(
+        <>
+            <p><a href="#product">Product</a></p>
+            <p><a href="#showcases">Showcases</a></p>
+            <p><a href="#license">License</a></p>
+            <p><a href="#resources">Resources</a></p>
+        </>
+    )
+/*'../../../public/index.html'*/
 function NavBar() {
+    const [toggleMenu, setToggleMenu] = useState(false)
+
     return (
         <div className="docwire__navbar">
             <div className="docwire__navbar-links_logo">
-                <img src={logo} alt="DocWire logo"/>
+            <a href='../../App.js'><img src={logo} alt="DocWire logo"/></a>
             </div>
             <div className="docwire__navbar-links_container">
-                <p><a href="#product">Product</a></p>
-                <p><a href="#showcases">Showcases</a></p>
-                <p><a href="#license">License</a></p>
-                <p><a href="#resources">Resources</a></p>
+                <Menu />
             </div>
             <div className='docwire__navbar-getintouch'>
                 <button>Get in touch</button>
             </div>
+            <div className="docwire__navbar-menu">
+                {toggleMenu
+                    ? <RiCloseLine color='white' size={27} onClick={()=> setToggleMenu(false)} />
+                    : <RiMenu3Line color='white' size={27} onClick={()=> setToggleMenu(true)} />
+                }
+                {toggleMenu && (
+                    <div className='docwire__navbar-menu_container scale-up-center'>
+                        <div className='docwire__navbar-menu_container-links'>
+                            <Menu />
+                        </div>
+                        <div className="docwire__navbar-menu_container-links-getintouch">
+                            <button>Get in touch</button>
+                        </div>
+                    </div>
+                )}
+            </div>
         </div>
-    )
+    );
 }
 
 export default NavBar;
