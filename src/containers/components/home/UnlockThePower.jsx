c9f5d30f3c5fb59738d162d4e7cd9c0494b8e221
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2024-02-22 18:15:58 +0100

    Add "unlock the power" section

---
+++ src/containers/components/home/UnlockThePower.jsx
@@ -0,0 +1,28 @@
+import "./unlockthepower.css";
+import React from "react";
+
+function UnlockThePower() {
+    return (
+        <div className="docwire__unlockthepower">
+            <div className="docwire__unlockthepower-content">
+                <div className="docwire__unlockthepower-content_subheader">
+                    Bespoke Software
+                </div>
+                <div className="docwire__unlockthepower-content_header">
+                    Unlock the Power of DocToText SDK
+                </div>
+                <div className="docwire__unlockthepower-content_section">
+                    Dealing with unstructured data can be a real hassle, but with Docwire's DocToText software, you can
+                    easily extract text from a variety of file formats. Our powerful C++ library enables lightning-fast text
+                    extraction from docx files, PDFs, and even pst/ost files. Our software is not only easy to use but also
+                    quick to deploy, saving you time and hassle. Whether you're dealing with legal documents, financial
+                    statements, or any other type of unstructured data, DocToText has got you covered. Try it today and
+                    experience the power of efficient and accurate text extraction.
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default UnlockThePower;
