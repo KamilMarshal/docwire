6478e5fca89592ec39830f271b8c0e8f57501ceb
Kamil Marszałek <108593228+KamilMarshal@users.noreply.github.com>
2024-03-16 23:38:57 +0100

    Deleting unnecessary console.log

--- src/pages/Showcases/Harpo.jsx
+++ src/pages/Showcases/Harpo.jsx
@@ -2,10 +2,8 @@ import {ShowcaseTemplate} from "../../containers";
 import {data} from './data'
 
 function Harpo() {
-    console.log(data[0].companyName)
     return (
-        <ShowcaseTemplate props={data[0]}
-            />
+        <ShowcaseTemplate props={data[0]}/>
     )
 }
 
